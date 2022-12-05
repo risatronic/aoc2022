f757afe521e2aec10fc2197869747b2706c76acc
risatronic <mcr.doig@gmail.com>
2022-12-04 18:25:36 -0800

    Day 4

--- dec04/p2.js
+++ dec04/p2.js
@@ -1,22 +1,18 @@
 const fs = require('fs')
 const input = fs.readFileSync('./input.txt', 'utf-8')
 const values = input.substring(0, input.lastIndexOf('\n')).split('\n')
+let total = 0
 
-const total = values.reduce((total, row) => {
+for(const row of values){
   const assignments = row.split(',')
   const assignment1 = assignments[0].split('-').map(x => parseInt(x))
   const assignment2 = assignments[1].split('-').map(x => parseInt(x))
-  const assignment1Array = Array.from({ length: assignment1[1] - assignment1[0] + 1 }, 
-                                      (_, x) => x + assignment1[0])
 
-  for(let i = assignment2[0]; i <= assignment2[1]; i++){
-    if(assignment1Array.includes(i)){
-      total++
-      break
-    }
+  if(assignment2[0] > assignment1[1] || assignment1[0] > assignment2[1]){
+    continue
   }
 
-  return total
-}, 0)
+  total++
+}
 
 console.log(`Overlapping assignments: ${total}`)
